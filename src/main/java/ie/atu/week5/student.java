6657f5c7176e04ac55a4d167a9af3595e89c9fd0
NathanPerry057 <G00394355@atu.ie>
2022-10-28 00:21:25 +0100

    Asks user to enter a student name

--- src/main/java/ie/atu/week5/student.java
+++ src/main/java/ie/atu/week5/student.java
@@ -1,4 +1,12 @@
 package ie.atu.week5;
 
+import java.util.Scanner;
+
 public class student {
+
+    public static void main(String[]  args) {
+
+        System.out.println("Please enter the name of student 1");
+        Scanner input = new Scanner(System.in);
+    }
 }
